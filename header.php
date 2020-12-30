ae1b122e7d93d47726d64de28ce4260dede663cf
Kev <kev@quirk.cc>
2020-12-30 09:38:22 +0000

    Initial commit

---
+++ header.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Nineteen_Quirky
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<script src="<?php echo esc_url( get_template_directory_uri() . '/js/name.js' ); ?>"></script>
+	<!-- START PLAUSIBLE ANALYTICS -->
+	<script async defer data-domain="kevq.uk" src="https://stats.kevq.uk/js/index.js"></script>
+	<!-- END PLAUSIBLE ANALYTICS -->
+
+	<!-- Trying out Goat Counter cuttently too -->
+		<script data-goatcounter="https://kev.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
+	<!-- End Goat counter code -->
+
+	<?php wp_head(); ?>
+</head>
+
+<body href="#top" <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'nineteen-quirky' ); ?></a>
+
+	<header id="masthead" class="site-header">
+		<div class="site-branding">
+			<?php
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?><span class="logo_dot"></span></a></h1>
+				<?php
+			else :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?><span class="logo_dot"></span></a></h1>
+				<?php
+			endif;
+			$nineteen_quirky_description = get_bloginfo( 'description', 'display' );
+			if ( $nineteen_quirky_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $nineteen_quirky_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			<?php endif; ?>
+		</div><!-- .site-branding -->
+
+		<div class="nav">
+			<nav id="site-navigation" class="main-navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( '🍔 Menu', 'nineteen-quirky' ); ?></button>
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
+					)
+				);
+				?>
+			</nav><!-- #site-navigation -->
+	</div>
+	</header><!-- #masthead -->
